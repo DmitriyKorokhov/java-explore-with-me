2c4db24e2f538ac38319924056f0e550f244f77a
Дмитрий <dkorokhov@yandex.ru>
2023-08-29 17:25:18 +0300

    1 этап Диплома. 2 сдача fix: исправил небольшие недочеты

--- ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/hit/service/HitServiceImpl.java
+++ ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/hit/service/HitServiceImpl.java
@@ -3,6 +3,7 @@ package ru.practicum.explorewithme.stats.server.hit.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.explorewithme.stats.dto.HitDto;
 import ru.practicum.explorewithme.stats.server.hit.storage.HitRepository;
 import ru.practicum.explorewithme.stats.server.hit.mapper.HitMapper;
@@ -10,12 +11,14 @@ import ru.practicum.explorewithme.stats.server.hit.model.Hit;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class HitServiceImpl implements HitService {
 
     private final HitRepository hitRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Hit addHit(HitDto hitDto) {
         Hit hit = HitMapper.toHit(hitDto);
         return hitRepository.save(hit);
