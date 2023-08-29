04540b7fa0689346b462c6040e4821813366f302
Дмитрий <dkorokhov@yandex.ru>
2023-08-29 17:34:32 +0300

    1 этап Диплома. 2 сдача fix: исправил небольшие недочеты

--- ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/hit/model/Hit.java
+++ ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/hit/model/Hit.java
@@ -3,10 +3,11 @@ package ru.practicum.explorewithme.stats.server.hit.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,11 +18,15 @@ public class Hit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "app")
+    @NotBlank(message = "App у Hit не должно быть пустым")
     private String app;
     @Column(name = "uri")
+    @NotBlank(message = "Uri у Hit не должно быть пустым")
     private String uri;
     @Column(name = "ip")
+    @NotBlank(message = "Ip у Hit не должно быть пустым")
     private String ip;
     @Column(name = "time_stamp")
+    @NotNull(message = "Time stamp у Hit должен существовать")
     private LocalDateTime timestamp;
 }
