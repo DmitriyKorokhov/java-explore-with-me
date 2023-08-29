85191dd58777a83324b7d4caa727aae1b0f9b350
Дмитрий <dkorokhov@yandex.ru>
2023-08-29 20:30:52 +0300

    1 этап Диплома. 2 сдача fix: добавление @DateTimeFormat

--- ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/formatter/DateTimeFormatConfig.java
+++ ewm-stat-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/formatter/DateTimeFormatConfig.java
@@ -5,15 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.practicum.explorewithme.stats.server.exception.ValidationDateException;
 
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Configuration
 public class DateTimeFormatConfig implements WebMvcConfigurer {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
-        registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        registrar.registerFormatters(registry);
+        if (registrar == null) {
+            throw new ValidationDateException("Дата должна быть задана");
+        }
+        try {
+            registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern(DATE_FORMAT));
+            registrar.registerFormatters(registry);
+        } catch (DateTimeParseException e) {
+            throw new ValidationDateException("Неверный формат даты");
+        }
     }
 }
