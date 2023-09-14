7c900d616c243e5e82286761c2e21edc5c11c332
Дмитрий <dkorokhov@yandex.ru>
2023-09-15 01:03:41 +0300

    3 этап Диплома. 1 сдача

--- ewm-main-service/src/main/java/ru/practicum/main_service/comment/service/impl/CommentsServiceAdminImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main_service/comment/service/impl/CommentsServiceAdminImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.main_service.comment.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
